38d7546d8b9413fa47539ac27afc8841c1ae7b01
Heiko Seeberger <mail@heikoseeberger.de>
2017-11-08 17:43:54 +0100

    Add skeleton for typed Account (groll:initial)

---
+++ src/main/scala/de/heikoseeberger/wat/typed/Account.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 Heiko Seeberger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.heikoseeberger.wat
+package typed
+
+import akka.typed.{ ActorRef, Behavior }
+import akka.typed.scaladsl.Actor
+
+/**
+  * A typed actor with an immutable behavior, i.e. one that doesn't close over mutable state.
+  */
+object Account {
+
+  // TODO Adapt untyped protocol to Akka Typed!
+
+  final case object GetBalance
+  final case class Balance(balance: Long)
+
+  final case class Deposit(amount: Long)
+  final case object Deposited
+
+  final case class Withdraw(amount: Long)
+  final case class InsufficientBalance(balance: Long)
+  final case object Withdrawn
+
+  def apply(balance: Long = 0): Behavior[Nothing] =
+    ???
+}
